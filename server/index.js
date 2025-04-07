b9b5a404c9b9eb9549a01a7cc805405cfdfbd87c
OlegR <oleg7309@inbox.lv>
2025-04-07 14:19:21 +0300

    slash in route

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.post('/api/chat',async (req, res) => {
 
 app.use(express.static(path.join(__dirname, '../build')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
